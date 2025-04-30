28eb51769f3070f1764a86ef5855fe6a36cb80b7
JacobAyala3 <JacobAyala3@my.untdallas.edu>
2025-04-30 16:42:45 -0500

    Fixes to the chances of weather effects so they actually happen more often

--- weather.cpp
+++ weather.cpp
@@ -8,8 +8,8 @@ WeatherEvent getRandomWeather(int timeStep) {
     if (timeStep % 5 != 0) return WeatherEvent::NONE;
 
     int roll = rand() % 10;
-    if (roll < 2) return WeatherEvent::TORNADO;   // 20% chance
-    if (roll < 5) return WeatherEvent::HEATWAVE;  // 30% chance
+    if (roll < 5) return WeatherEvent::TORNADO;   // 20% chance
+    if (roll < 9) return WeatherEvent::HEATWAVE;  // 30% chance
     return WeatherEvent::NONE;
 }
 
